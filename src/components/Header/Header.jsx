7824b98306f53c561ddf56fe96f6bf2bdade46b8
Ilia Krylatykh <iliakrylatykh@gmail.com>
2023-06-12 11:12:27 +0400

    check

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,10 @@
+import { Button } from '../ui/Button/Button'
 import './Header.scss'
 
 export const Header = () => {
-	return <nav className='header'>Hello</nav>
+	return (
+		<nav className='header'>
+			<Button>Close</Button>
+		</nav>
+	)
 }
